5f3205ac9c77149c6cd4453cd7fe7126329c65a8
BaBeuloula <babeuloula@gmail.com>
2014-10-21 16:10:11 +0200

    Create Autoloader.php

---
+++ Autoloader.php
@@ -0,0 +1,28 @@
+<?php
+
+    Autoloader::Register();
+
+    class Autoloader {
+
+        public static function Register () {
+            if (function_exists('__autoload')) {
+                spl_autoload_register('__autoload');
+            }
+
+            return spl_autoload_register(array('Autoloader', 'Load'));
+        }
+
+        public static function Load ($className) {
+            if (class_exists($className, false)) {
+                return false;
+            }
+
+            $classFilePath = __DIR__ . DIRECTORY_SEPARATOR . $className . '.php';
+
+            if ((file_exists($classFilePath) === false) || (is_readable($classFilePath) === false)) {
+                return false;
+            }
+
+            require($classFilePath);
+        }
+    }
